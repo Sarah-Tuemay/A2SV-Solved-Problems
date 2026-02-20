7cda359df66903a6d4a63c1e48281349814b3e8b
Sarah <saratuemay12@gmail.com>
2026-02-20 21:10:42 +0300

    Create H-Index.py

---
+++ H-Index.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse=True)
+        # print(citations)
+        h_index = 0
+        for i in range(len(citations)):
+            if i + 1 <= citations[i]:
+                h_index = i+1
+
+        return h_index
